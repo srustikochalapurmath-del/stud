6d61de536dbc5a966150f57f42ca7d7ece6c96f2
srustikochalapurmath-del <srustikochalapurmath@gmail.com>
2025-12-02 12:24:39 +0530

    Update test_student.py

--- test_student.py
+++ test_student.py
@@ -1,1 +1,12 @@
+from student import student_details
+
+def test_student_details():
+    expected_output = (
+        "studentid: e1101\n"
+        "studentname: Alice\n"
+        "student course: BCA\n"
+        "student academic year: 2024"
+    )
+
+    assert student_details("e1101", "Alice", "BCA", 2024) == expected_output
 
